cb2c3178b1be079f795467760b0af8fbf89ae98c
Samin <samincgs@gmail.com>
2023-06-11 19:15:37 +0600

    Added a score tracker and allowed enemies to explode with some nice animation

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-console.log(gsap);
 // CANVAS
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-
+const scoreEl = document.querySelector('#score');
 // CANVAS WIDTH & HEIGHT
 canvas.width = innerWidth;
 canvas.height = innerHeight;
@@ -76,11 +75,43 @@ class Enemy {
   }
 }
 
+const friction = 0.99;
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01;
+  }
+}
+
 // USING THE PLAYER CLASS
 const player = new Player(x, y, 12, '#fff');
 
 const projectiles = [];
 const enemies = [];
+const particles = [];
 
 const spawnEnemies = function () {
   setInterval(() => {
@@ -108,12 +139,20 @@ const spawnEnemies = function () {
 
 // ANIMATION LOOP FUNCTION
 let animationId;
+let score = 0;
 const animate = function () {
   animationId = requestAnimationFrame(animate);
   c.fillStyle = 'rgba(0,0,0,0.1)';
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
 
+  particles.forEach((part, i) => {
+    if (part.alpha <= 0) {
+      particles.splice(i, 1);
+    } else {
+      part.update();
+    }
+  });
   projectiles.forEach((proj, index) => {
     proj.update();
 
@@ -123,7 +162,7 @@ const animate = function () {
       proj.y + proj.radius < 0 ||
       proj.y - proj.radius > canvas.height
     ) {
-      //off the screen on the left
+      //off the screen all 4 sides
       setTimeout(() => {
         projectiles.splice(index, 1);
       }, 0);
@@ -142,9 +181,21 @@ const animate = function () {
     projectiles.forEach((proj, pi) => {
       const distance = Math.hypot(proj.x - enemy.x, proj.y - enemy.y);
       if (distance - enemy.radius - proj.radius < 1) {
-        //when projectile touches enemy
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          //creating explosions
+          particles.push(
+            new Particle(proj.x, proj.y, Math.random() * 2, enemy.color, {
+              x: (Math.random() - 0.5) * (Math.random() * 7),
+              y: (Math.random() - 0.5) * (Math.random() * 7),
+            })
+          );
+        }
 
+        //when projectile touches enemy
         if (enemy.radius - 10 > 5) {
+          //increase score
+          score += 50;
+          scoreEl.textContent = score;
           gsap.to(enemy, {
             radius: enemy.radius - 10,
           });
@@ -152,6 +203,9 @@ const animate = function () {
             projectiles.splice(pi, 1);
           }, 0);
         } else {
+          //increase score
+          score += 150;
+          scoreEl.textContent = score;
           setTimeout(() => {
             enemies.splice(i, 1);
             projectiles.splice(pi, 1);
