8d7c2e4a5d5724d5718ddd04f0cee5767e2faf27
Samin <samincgs@gmail.com>
2023-06-11 15:44:08 +0600

    Made game faster by removing all projectiles outside the canvas and also randomized the enemy colors

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ class Enemy {
 }
 
 // USING THE PLAYER CLASS
-const player = new Player(x, y, 30, 'blue');
+const player = new Player(x, y, 12, '#fff');
 
 const projectiles = [];
 const enemies = [];
@@ -93,7 +93,8 @@ const spawnEnemies = function () {
       x = Math.random() * canvas.width;
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
-    const color = 'green';
+    const hue = Math.random() * 360;
+    const color = `hsl(${hue}, 50%, 50%)`;
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x); //right angle of a triangle to the mouse click
     const velocity = {
       x: Math.cos(angle),
@@ -105,19 +106,40 @@ const spawnEnemies = function () {
 };
 
 // ANIMATION LOOP FUNCTION
+let animationId;
 const animate = function () {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  animationId = requestAnimationFrame(animate);
+  c.fillStyle = 'rgba(0,0,0,0.1)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
 
-  projectiles.forEach((proj) => {
+  projectiles.forEach((proj, index) => {
     proj.update();
+
+    if (
+      proj.x + proj.radius < 0 ||
+      proj.x - proj.radius > canvas.width ||
+      proj.y + proj.radius < 0 ||
+      proj.y - proj.radius > canvas.height
+    ) {
+      //off the screen on the left
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
+
   enemies.forEach((enemy, i) => {
     enemy.update();
+
+    const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    if (distance - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId);
+    }
+
     projectiles.forEach((proj, pi) => {
       const distance = Math.hypot(proj.x - enemy.x, proj.y - enemy.y);
-
       if (distance - enemy.radius - proj.radius < 1) {
         //when projectile touches enemy
         setTimeout(() => {
@@ -133,11 +155,11 @@ const animate = function () {
 addEventListener('click', (e) => {
   const angle = Math.atan2(e.clientY - y, e.clientX - x); //right angle of a triangle to the mouse click
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle),
+    x: Math.cos(angle) * 5,
+    y: Math.sin(angle) * 5,
     //gives me x and y velocity using the angle variable
   };
-  projectiles.push(new Projectile(x, y, 5, 'red', velocity));
+  projectiles.push(new Projectile(x, y, 5, 'white', velocity));
 });
 
 // CALLING ANIMATION LOOP FUNCTION
